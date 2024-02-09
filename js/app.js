b1929f63e10a44727669ce0efa771de6dd2c7603
ejesh007 <153279912+ejesh007@users.noreply.github.com>
2024-02-09 10:41:53 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 
 console.clear();
+
+ 
+ 
+const subDiscsub = document.querySelector('.sub-disc1sub');
+ 
+
+ 
+const tl = new TimelineMax();
+tl.from(".line span", 1.8, {
+  y: 100,
+  ease: "power4.out",
+  delay: 1,
+  skewY: 7,
+  stagger: {
+    amount: 0.3
+  }
+}, "-=1") 
+  .fromTo(".s-logo", 1, {opacity:0 }, {opacity:1, ease: Power2.easeInOut},"-=1")
+  .fromTo(".camp-logo", 1, {opacity:0 }, {opacity:1, ease: Power2.easeInOut},"-=1")
+  .fromTo(".building-title", 1, {y:"200%" }, {y:"0%", ease: Power2.easeInOut},"-=1")
  
 
 
- 
 
 
 
@@ -24,9 +43,9 @@ gsap.to('.center-build',{
 
 
 ScrollTrigger.matchMedia({
-  "(min-width: 1000px)": function () {
+  "(max-width: 1000px)": function () {
 gsap.to('.golf-man',{ 
-  y:-457,
+  y:-350,
   scale:1,
   scrollTrigger:{
   trigger:".golf-baner-sec",
@@ -46,31 +65,30 @@ return function() {
   }
   });
 
-
-
 ScrollTrigger.matchMedia({
-  "(max-width: 1000px)": function () {
-    gsap.to('.golf-man',{ 
-      y:-300,
-      scale:1,
-      
-      scrollTrigger:{
-      trigger:".golf-baner-sec",
-      start: "center center",
-      end: "+=1000 center",
-      scrub:1,
-      pin:true,
-      toggleActions:"none none none none",   
+  "(min-width: 1000px)": function () {
+gsap.to('.golf-man',{ 
+  y:-597,
+  scale:1,
+  scrollTrigger:{
+  trigger:".golf-baner-sec",
+  start: "center center",
+  end: "+=1000 center",
+  scrub:1,
+  pin:true,
+  toggleActions:"none none none none",   
  
-    }
-    }
-    );
-    return function() {
-      animation.kill();
-      console.log('kill it max-width');
-      }
-      }
-      });
+}
+}
+);
+return function() {
+  animation.kill();
+  console.log('kill it max-width');
+  }
+  }
+  });
+
+ 
 
 
 
